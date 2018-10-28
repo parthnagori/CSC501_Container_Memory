f1c3b26103bdf7db4ac5e7446a9a823af16e0a6a
pnagori <pnagori@ncsu.edu>
2018-10-28 19:19:01 -0400

    mc

--- kernel_module/src/ioctl.c
+++ kernel_module/src/ioctl.c
@@ -236,11 +236,11 @@ int memory_container_mmap(struct file *filp, struct vm_area_struct *vma)
     copy_from_user(&temp_vma, vma, sizeof(struct vm_area_struct));
     
     //Setting calling thread's associated cid
-    unsigned long long int cid = temp_vma.cid;
+    // unsigned long long int cid = temp_vma.cid;
     unsigned long long int oid = temp_vma.oid;
     //Setting calling thread's associated pid
     int pid = current->pid;
-    printk("\nInside mmap : CID -> %llu --- PID -> %d --- OID -> %llu", cid, pid, oid);
+    printk("\nInside mmap : PID -> %d --- OID -> %llu", pid, oid);
 
     return 0;
 }
